629c1afe8a77496b5e828690d73057422704f06c
Gulkat <Kateguleshova@gmail.com>
2022-06-03 12:35:24 +0300

    Warning card in column In Progress

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,10 @@ let containerTdo = document.querySelector('.dashboard__cards-todo');
 let containerInProgress = document.querySelector('.dashboard__cards-inProgress');
 let containerDone = document.querySelector('.dashboard__cards-done');
 
-dragula([containerTdo, containerInProgress, containerDone]);
+let drake = dragula([containerTdo, containerInProgress, containerDone]);
 
-
+drake.on('drop', function(el, target, source, sibling) {
+    if (target === containerInProgress && target.children.length >= 6) {
+        $('.ui.modal.pop-up__inprogress').modal({blurring: true}).modal('show');
+    }
+});
