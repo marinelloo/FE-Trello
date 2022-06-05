041489b031e500e4a5e5954417878520c8c3dfa4
Loki4ch <98488167+Loki4ch@users.noreply.github.com>
2022-06-05 17:14:28 +0300

    Philippov delete pop ups complete

--- src/js/main.js
+++ src/js/main.js
@@ -20,13 +20,18 @@ drake.on('drop', function(el, target, source, sibling) {
 });
 
 
-// Philippov Pop ups
+// Pop ups
 
 let cardsTodos = document.querySelectorAll(".card__todo");
 let trash = document.querySelector('.trash');
 let btnDeleteAll = document.querySelector('.btn__delete');
+let btnDeleteConfirm = document.querySelector('.btn--dark');
+let dashboardDone = document.querySelector('.dashboard__cards-done');
+btnDeleteConfirm.addEventListener("click", (event) => {
+	dashboardDone.innerHTML = '';
+});
 btnDeleteAll.addEventListener("click", (event) => {
-	
+	$('.ui.modal.pop-up__delete-all').modal({blurring: true}).modal('show');
 });
 for(let i = 0; i < cardsTodos.length; i++) {
 	cardsTodos[i].addEventListener("click", (event) => {
@@ -35,48 +40,3 @@ for(let i = 0; i < cardsTodos.length; i++) {
 		}
 	});
 }
-
-
-
-
-
-// let dashboard = document.querySelector('.wrapper__dashboard');
-// let trash = dashboard.querySelector('.trash');
-// let cardsTodos = dashboard.querySelectorAll('.card__todo');
-// let btnDeleteAll = document.querySelector('.btn__delete');
-// dashboard.addEventListener('click', (event) => {
-// 	const arr = [];
-// 	for (let i = 0; i < cardsTodos.length; i++) {
-// 		arr.push(cardsTodos[i]);
-// 	}
-// 	console.log(arr);
-// 	let o = arr.indexOf(event.target);
-// 	cardsTodos[o].innerHTML = '';
-	// if(JSON.stringify(event.target) === JSON.stringify(trash)) {
-		
-	// }
-// })
-
-
-
-
-// dashboardTodo.addEventListener('click', (event) => {
-// 	if (event.target === cardTodoDelete[event.target]) {
-// 	cardsTodos[event.target].innerHTML = ''}});
-// let cardTodo = document.querySelector('.card__todo');
-// console.log(cardTodo);
-// cardTodo.addEventListener('click', (event) => {
-// 	if (event.target === cardTodoDelete) {
-// 		alert('hello');
-// 	}});
-
-
-
-//   for (let elem of elements) {
-//     alert(elem.innerHTML); // "тест", "пройден"
-//   }
-// const btnDeleteAll = document.querySelector('.btn__delete');
-// btnDeleteAll.addEventListener('click', (event) => {
-// 	if (event.target === btnDeleteAll) {
-// 	alert('hello');
-// }});
