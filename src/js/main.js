5ff6b7e3b49aec65102c872eb226efbf7cf77dd0
Loki4ch <98488167+Loki4ch@users.noreply.github.com>
2022-06-05 16:34:46 +0300

    delete one done

--- src/js/main.js
+++ src/js/main.js
@@ -22,4 +22,61 @@ drake.on('drop', function(el, target, source, sibling) {
 
 // Philippov Pop ups
 
-let 
+let cardsTodos = document.querySelectorAll(".card__todo");
+let trash = document.querySelector('.trash');
+let btnDeleteAll = document.querySelector('.btn__delete');
+btnDeleteAll.addEventListener("click", (event) => {
+	
+});
+for(let i = 0; i < cardsTodos.length; i++) {
+	cardsTodos[i].addEventListener("click", (event) => {
+		if(JSON.stringify(event.target) === JSON.stringify(trash)) {
+			cardsTodos[i].innerHTML = '';
+		}
+	});
+}
+
+
+
+
+
+// let dashboard = document.querySelector('.wrapper__dashboard');
+// let trash = dashboard.querySelector('.trash');
+// let cardsTodos = dashboard.querySelectorAll('.card__todo');
+// let btnDeleteAll = document.querySelector('.btn__delete');
+// dashboard.addEventListener('click', (event) => {
+// 	const arr = [];
+// 	for (let i = 0; i < cardsTodos.length; i++) {
+// 		arr.push(cardsTodos[i]);
+// 	}
+// 	console.log(arr);
+// 	let o = arr.indexOf(event.target);
+// 	cardsTodos[o].innerHTML = '';
+	// if(JSON.stringify(event.target) === JSON.stringify(trash)) {
+		
+	// }
+// })
+
+
+
+
+// dashboardTodo.addEventListener('click', (event) => {
+// 	if (event.target === cardTodoDelete[event.target]) {
+// 	cardsTodos[event.target].innerHTML = ''}});
+// let cardTodo = document.querySelector('.card__todo');
+// console.log(cardTodo);
+// cardTodo.addEventListener('click', (event) => {
+// 	if (event.target === cardTodoDelete) {
+// 		alert('hello');
+// 	}});
+
+
+
+//   for (let elem of elements) {
+//     alert(elem.innerHTML); // "тест", "пройден"
+//   }
+// const btnDeleteAll = document.querySelector('.btn__delete');
+// btnDeleteAll.addEventListener('click', (event) => {
+// 	if (event.target === btnDeleteAll) {
+// 	alert('hello');
+// }});
