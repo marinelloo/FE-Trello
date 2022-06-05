a045ca0e7393c6c0778aaace75fd25f62f2891d5
marinelloo <maryna.chertkoieva@gmail.com>
2022-06-05 17:03:36 +0200

    fix delete

--- src/js/main.js
+++ src/js/main.js
@@ -27,16 +27,20 @@ let trash = document.querySelector('.trash');
 let btnDeleteAll = document.querySelector('.btn__delete');
 let btnDeleteConfirm = document.querySelector('.btn--dark');
 let dashboardDone = document.querySelector('.dashboard__cards-done');
+
 btnDeleteConfirm.addEventListener("click", (event) => {
 	dashboardDone.innerHTML = '';
 });
+
 btnDeleteAll.addEventListener("click", (event) => {
 	$('.ui.modal.pop-up__delete-all').modal({blurring: true}).modal('show');
 });
+
+
 for(let i = 0; i < cardsTodos.length; i++) {
 	cardsTodos[i].addEventListener("click", (event) => {
 		if(JSON.stringify(event.target) === JSON.stringify(trash)) {
-			cardsTodos[i].innerHTML = '';
+			cardsTodos[i].remove();
 		}
 	});
 }
