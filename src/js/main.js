b789201f85e1d10a93c338a42750a788ea86a3ca
Yaroslavrazhko <yaroslav.razhko@gmail.com>
2022-06-05 10:37:02 +0200

    JS part

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const btnAdd = document.getElementById('btn-add');
 
 btnAdd.addEventListener('click', () => {
-    $('.ui.modal.add__todo').modal({blurring: true}).modal('show');
+    $('.ui.modal.add__todo').modal({ blurring: true }).modal('show');
     $('.ui.dropdown').dropdown();
 })
 
@@ -13,8 +13,106 @@ let containerDone = document.querySelector('.dashboard__cards-done');
 
 let drake = dragula([containerTdo, containerInProgress, containerDone]);
 
-drake.on('drop', function(el, target, source, sibling) {
+drake.on('drop', function (el, target, source, sibling) {
     if (target === containerInProgress && target.children.length >= 6) {
-        $('.ui.modal.pop-up__inprogress').modal({blurring: true}).modal('show');
+        $('.ui.modal.pop-up__inprogress').modal({ blurring: true }).modal('show');
     }
 });
+
+//Add New card
+let todos = [];
+
+const TodoConstructor = function (todoTitle, todoDescription, assignedPerson) {
+    this.todoTitle = todoTitle;
+    this.todoDescription = todoDescription;
+    this.assignedPerson = assignedPerson;
+}
+
+
+//Create Todo
+const createTodo = (todoTitle, todoDescription, assignedPerson) => {
+    const todoCase = document.createElement("div");
+    todoCase.className = "card__todo";
+
+    const cardTop = document.createElement("div");
+    cardTop.className = "card_top";
+
+    const todoTitleHead = document.createElement("h3");
+    todoTitleHead.className = "card__todo-title title4";
+    const todoTitleText = document.createTextNode(todoTitle);
+
+    const todoDate = document.createElement("div");
+    const date = new Date().toLocaleTimeString();
+    todoDate.className = "card__todo-title";
+
+    const todoDescriptionCard = document.createElement("div");
+    todoDescriptionCard.className = "todo-description";
+    const todoDescriptionText = document.createTextNode(todoDescription);
+
+
+    todoCase.append(cardTop);
+    cardTop.append(todoTitleHead);
+    todoTitleHead.append(todoTitleText);
+    cardTop.append(todoDate);
+    todoDate.append(date);
+    todoDescriptionCard.append(todoDescriptionText);
+    todoCase.append(todoDescriptionCard);
+
+    const cardBottom = document.createElement("div");
+    cardBottom.className = "card_bottom";
+
+    const user = document.createElement("div");
+    user.className = "user";
+
+    const todoAuthor = document.createElement("img");
+    todoAuthor.className = "card__todo-author";
+
+    const todoUserName = document.createElement("p");
+    const todoUserNameText = document.createTextNode(assignedPerson);
+    todoUserName.className = "todo__user-name";
+
+    const cardEdit = document.createElement("div");
+    cardEdit.className = "card__todo-edit";
+
+    const linkEdit = document.createElement("a");
+    //a.href = "#";
+    linkEdit.className = "card__todo-edit";
+    const linkEditPicture = document.createElement("i");
+    linkEditPicture.className = "edit icon";
+
+    const linkDelete = document.createElement("a");
+    //a.href = "#";
+    linkDelete.className = "card__todo-delete";
+    const linkDeletePicture = document.createElement("i");
+    linkDeletePicture.className = "trash alternate icon";
+
+    todoCase.append(cardBottom);
+    cardBottom.append(user);
+    user.append(todoAuthor);
+    user.append(todoUserName);
+    cardBottom.append(cardEdit);
+    cardEdit.append(linkEdit);
+    cardEdit.append(linkDelete);
+    linkEdit.append(linkEditPicture);
+    linkDelete.append(linkDeletePicture);
+    todoUserName.append(todoUserNameText);
+
+    return todoCase;
+}
+
+//Get access
+const approveBtn = document.getElementById("approveBtn");
+const cardTodo = document.getElementById("todoCase");
+const inputTitle = document.getElementById('inputTitle');
+const inputDescription = document.getElementById('inputDescription');
+const assignedPerson = document.getElementById('assignedPerson');
+
+
+approveBtn.addEventListener('click', () => {
+    //const todo = new TodoConstructor(inputTitle.value, inputDescription.name, assignedPerson);
+    //cardTodo.append(createTodo(inputTitle.value, inputDescription.name, assignedPerson));
+    const todo = new TodoConstructor(inputTitle.value, document.getElementById('inputDescription').value, assignedPerson);
+    cardTodo.append(createTodo(inputTitle.value, document.getElementById('inputDescription').value, assignedPerson));
+    todos.push(todo);
+    //input.value = "";
+})
